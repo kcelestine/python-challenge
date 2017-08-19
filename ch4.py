8f22f5d8efdf78471d924fc133b359a612e2d78e
Khadijah <khadijah.celesstine@gmail.com>
2017-08-19 13:14:25 -0400

    ok starting ch4

---
+++ ch4.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+f = open('4_help', 'r')
+lines = f.readlines()
+
+d = dict()
+
+for line in lines:
+	line = list(line)
+	for c in line:
+		d[c] = d.get(c, 0) +1
+
+print d
+
+
+for k,v in d.items():
+	print("{:<12} {:<12}".format(k, v))
+
+'''
+counts = dict()
+for i in items:
+  counts[i] = counts.get(i, 0) + 1
+'''
