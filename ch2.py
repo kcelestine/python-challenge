e273232051ceaf93dbec32904df326bfceea8a86
Khadijah <khadijah.celesstine@gmail.com>
2017-08-19 12:49:51 -0400

    solved ch2

--- ch2.py
+++ ch2.py
@@ -1,6 +1,8 @@
 word = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
 
+word2 = "http://www.pythonchallenge.com/pc/def/map.html"
 
+word3 = "map"
 original = word
 word = list(word)
 word2 = word
@@ -40,7 +42,20 @@ def f(x):
         'z': 'b',
     }[x]
 
+a = []
 for letter in word:
-	print f(letter)
+	a.append(f(letter))
 
-print word
+print ''.join(a)
+
+a = []
+for letter in word2:
+	a.append(f(letter))
+
+print ''.join(a)
+
+a = []
+for letter in word3:
+	a.append(f(letter))
+
+print ''.join(a)
