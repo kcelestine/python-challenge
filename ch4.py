2ee1eeaf041b819e5374f603518cdd92b691e8e5
Khadijah <khadijah.celesstine@gmail.com>
2017-08-19 14:11:08 -0400

    messed up

--- ch4.py
+++ ch4.py
@@ -5,39 +5,16 @@ import re
 f = open('4_help', 'r')
 lines = f.readlines()
 
-d = dict()
-
-for line in lines:
-	line = list(line)
-	for c in line:
-		d[c] = d.get(c, 0) +1
-values = []
-for k,v in d.items():
-	values.append(v)
-
-values.sort()
-
-for v in values:
-	print v
-
-print values
-	
-def check(pattern, text):
+count = 0
+def check(text):
+	pattern="([A-Z][A-Z][A-Z][a-z][A-Z][A-Z][A-Z])"
 	if re.search(pattern,  text):
-		return 'found a match!'
+		return True
 	else:
-		return 'no match!'
+		return False
 
-word = "AAAbAAA"
-word2 = "ababaA3"
+lines = []
+for line in lines:
+	if(check(line)):
+		print line
 
-pattern="([A-Z][A-Z][A-Z][a-z][A-Z][A-Z][A-Z])"
-
-print check(pattern, word)
-print check(pattern, word2)
-'''
-for each line
-	build word
-	if word is 7 chars
-	print word
-'''
